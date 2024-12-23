0230b8df02215d1840c2c1958fe41e7e28056427
Ayana Gail Soriano <ayana.gail924@gmail.com>
2024-12-23 22:36:48 +0800

    Delete LA/4 directory

--- LA/4/3/LA#3.py
+++
@@ -1,9 +0,0 @@
-class character:
-    def __init__(self, name):
-        self.name = name
-    
-    def describe(self, role):
-        print(f"{self.name} is a {role} hero.")
-
-ml = character("Saber")
-ml.describe("Assasin")
